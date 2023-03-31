6802dfd80f0d1e933860eb5caa86156ebc585013
Jakub Gerlee <Jakub_Gerlee@epam.com>
2023-03-31 15:40:17 +0200

    Added api versioning to project.

--- Social.Api/Controllers/PostController.cs
+++ Social.Api/Controllers/PostController.cs
@@ -4,7 +4,7 @@ using Social.Domain.Models;
 namespace Social.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/v1/[controller]")]
 public class PostController : Controller
 {
 	[HttpGet]
--- Social.Api/Program.cs
+++ Social.Api/Program.cs
@@ -1,11 +1,23 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1,0);
+    config.AssumeDefaultVersionWhenUnspecified = true; //if you don't specify version, use defualt
+    config.ReportApiVersions = true; //if you have a couple of versions will send back supported headers in response
+    config.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
