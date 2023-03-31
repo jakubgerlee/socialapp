826aff3940f2a2ae1dd205a48f2e317178a7aef0
Jakub Gerlee <Jakub_Gerlee@epam.com>
2023-03-31 14:43:42 +0200

    Added small Post endpoit

---
+++ Social.Api/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Social.Domain.Models;
+
+namespace Social.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PostController : Controller
+{
+	[HttpGet]
+	[Route("{id}")]
+	public IActionResult GetById(int id)
+	{
+		var post = new Post { Id = id, Text = "Hello, world" };
+
+		return Ok(post);
+	}
+}
